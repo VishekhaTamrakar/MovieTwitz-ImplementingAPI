22986da107bc64ae786d656c3bf6d3db03c22455
Vishekha <vtamrakar@unomaha.edu>
2019-07-24 09:00:03 -0500

    HomePage

--- mtapp/urls.py
+++ mtapp/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+
+
 from . import views
 from django.urls import path
 
@@ -9,8 +12,13 @@ urlpatterns = [
     path('', views.home, name='home'),
     url(r'^home/$', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
+    path('About/', views.About, name='About'),
+    path('Contact/', views.Contact, name='Contact'),
     path('movie_list', views.movie_list, name='movie_list'),
     path('movie/<int:pk>/edit/', views.movie_edit, name='movie_edit'),
     path('movie/<int:pk>/delete/', views.movie_delete, name='movie_delete'),
     path('movie/create/', views.movie_new, name='movie_new'),
+
+
 ]
+
