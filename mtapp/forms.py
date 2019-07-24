10d3ddfec433a38e4c8b02463ac1e8d070fc54c4
harry2491 <hharwinderkaur@unomaha.edu>
2019-07-23 21:22:52 -0500

    Adding Movie CRUD

--- mtapp/forms.py
+++ mtapp/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Movie
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
+
+class MovieForm(forms.ModelForm):
+    class Meta:
+        model = Movie
+        fields = ('name', 'release_date', 'imdb_id', 'genre', 'directed_by', 'runtime', 'notable_casts', 'rating', 'disc_price', 'review')
